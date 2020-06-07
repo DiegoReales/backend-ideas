36dfeb0324c7bcef4bb9b895c4f9bb63fce6fadf
Diego Reales <diego.reales@hotmail.com>
2020-06-07 14:49:42 -0500

    ADD: rutas agregadas.

--- src/startup/container.js
+++ src/startup/container.js
@@ -11,7 +11,7 @@ const { HomeService, UserService, IdeaService, CommentService } = require("../se
 const { HomeController, UserController, IdeaController, CommentController } = require("../controllers");
 
 // Routes
-const { HomeRoutes } = require("../routes/index.routes");
+const { HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes } = require("../routes/index.routes");
 const Routes = require('../routes');
 
 // Models
@@ -42,6 +42,9 @@ container
 	})
 	.register({
 		HomeRoutes: asFunction(HomeRoutes).singleton(),
+		UserRoutes: asFunction(UserRoutes).singleton(),
+		IdeaRoutes: asFunction(IdeaRoutes).singleton(),
+		CommentRoutes: asFunction(CommentRoutes).singleton(),
 	})
 	.register({
 		User: asValue(User),
